482b9ae253278ac7c312473b609d4c34c9f882eb
bambiker <50655137+bambiker@users.noreply.github.com>
2024-03-15 07:51:56 +0200

    Update map.js

--- map.js
+++ map.js
@@ -174,59 +174,76 @@ ws10=json.hourly.wind_speed_10m[hour-1]/3.6; //array start at zero
 
 //// time to go to 20m + time to go horizontaly
 //    window.alert(ws80+ ' m/s '+ wd80 + ' de2222grees');
-    speedup=6/2;
-    speedhorizontal=18/2;
+
+    speedup=document.getElementById('asc').value/document.getElementById('payload').value;
+    speeddown=document.getElementById('des').value/document.getElementById('payload').value;
+    speedhorizontal=document.getElementById('hor').value/document.getElementById('payload').value;
    
-    drag=0.06
+    drag=document.getElementById('drag').value
+   
+    heights = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+    ws = []
+    wd = []
+    timeupdown = []
+    timehor = []
+    timehorb = []
+    for (i=0;i<heights.length; i++) {
+    if (heights[i]<80)
+            {
+            ws[i]=ws10*(80-heights[i])/70+ws80*(heights[i]-10)/70
+            wd[i]=wd10*(80-heights[i])/70+wd80*(heights[i]-10)/70
+            }
+        else if (heights[i]==80)
+            {
+            ws[i]=ws80
+            wd[i]=wd80
+            }
+            else if (heights[i]==120)
+   {
+   ws[i]=ws120
+   wd[i]=wd120  
+   }
+   else
+   {
+   ws[i]=ws80*(120-heights[i])/40+ws120*(heights[i]-80)/40
+   wd[i]=wd80*(120-heights[i])/40+wd120*(heights[i]-80)/40
+   }
+timeupdown[i] = (heights[i]/speedup)+(heights[i]/speeddown)
+diffangle=(wd[i]-dronedegrees)/180*Math.PI
+angle = Math.cos(diffangle)*drag
+timehor[i] = dist /  (speedhorizontal+ws[i]*angle)
+timehorb[i] = dist / (speedhorizontal-ws[i]*angle)
+    }
 
-    height=10
-    diffangle=(wd10-dronedegrees)/360*Math.PI
-    angle = Math.cos(diffangle)*drag
-    timeup10=2.*height/speedup
-    timehorizontalws10 = dist / (speedhorizontal+ws10*angle)
-    timehorizontalws10b = dist / (speedhorizontal-ws10*angle)
+    minhor = 0
+    minhorb = 0
+    for (i=0;i<heights.length; i++) {
+        if (timeupdown[i]+timehor[i]<timeupdown[minhor]+timehor[minhor])
+            minhor=i
+        if (timeupdown[i]+timehorb[i]<timeupdown[minhorb]+timehorb[minhorb])
+            minhorb=i
+    }
+       
+    document.getElementById('heightfore').innerHTML = heights[minhor].toFixed(2)
+    document.getElementById('heightback').innerHTML = heights[minhorb].toFixed(2)
+    document.getElementById('distance').innerHTML = dist.toFixed(2)
+    document.getElementById('dronedir').innerHTML = dronedegrees.toFixed(2)
+    document.getElementById('winddir').innerHTML = wd80.toFixed(2)
+    document.getElementById('timenowind').innerHTML = (timeupdown[0]+(dist / speedhorizontal)).toFixed(2)
+    document.getElementById('timefore20').innerHTML = (timeupdown[0]+timehor[0]).toFixed(2)
+    document.getElementById('timeback20').innerHTML = (timeupdown[0]+timehorb[0]).toFixed(2)
+    document.getElementById('timefore80').innerHTML = (timeupdown[6]+timehor[6]).toFixed(2)
+    document.getElementById('timeback80').innerHTML = (timeupdown[6]+timehorb[6]).toFixed(2)
+    document.getElementById('timefore120').innerHTML = (timeupdown[10]+timehor[10]).toFixed(2)
+    document.getElementById('timeback120').innerHTML = (timeupdown[10]+timehorb[10]).toFixed(2)
 
-    height=80
-    diffangle=(wd80-dronedegrees)/360*Math.PI
-    angle = Math.cos(diffangle)*drag
-    timeup80=2.*height/speedup
-    timehorizontal = dist / speedhorizontal
-    timehorizontalws80 = dist / (speedhorizontal+ws80*angle)
-    timehorizontalws80b = dist / (speedhorizontal-ws80*angle)
+    travel20=2*timeupdown[0]+timehor[0]+timehorb[0]
+    travelopt=timeupdown[minhor]+timehor[minhor]+timeupdown[minhorb]+timehorb[minhorb]
+    //window.alert(2*timeupdown[0]+','+timehor[0]+timehorb[0]+'('+iminfore+','+iminback)
 
-    height=120
-    diffangle=(wd120-dronedegrees)/360*Math.PI
-    angle = Math.cos(diffangle)*drag
-    timeup120=2.*height/speedup
-    timehorizontalws120 = dist / (speedhorizontal+ws120*angle)
-    timehorizontalws120b = dist / (speedhorizontal-ws120*angle)
-    duration80=(timeup80+timehorizontalws80)+(timeup80+timehorizontalws80b)
-    if ((timeup10+timehorizontalws10)<(timeup80+timehorizontalws80) && (timeup10+timehorizontalws10)<(timeup120+timehorizontalws120))
-durationmin=timeup10+timehorizontalws10
-    if ((timeup80+timehorizontalws80)<(timeup10+timehorizontalws10) && (timeup80+timehorizontalws80)<(timeup120+timehorizontalws120))
-durationmin=timeup80+timehorizontalws80
-    if ((timeup120+timehorizontalws120)<(timeup10+timehorizontalws10) && (timeup120+timehorizontalws120)<(timeup80+timehorizontalws80))
-durationmin=timeup120+timehorizontalws120
-    if ((timeup10+timehorizontalws10b)<(timeup80+timehorizontalws80b) && (timeup10+timehorizontalws10b)<(timeup120+timehorizontalws120b))
-durationmin=durationmin+timeup10+timehorizontalws10b
-    if ((timeup80+timehorizontalws80b)<(timeup10+timehorizontalws10b) && (timeup80+timehorizontalws80b)<(timeup120+timehorizontalws120b))
-durationmin=durationmin+timeup80+timehorizontalws80b
-    if ((timeup120+timehorizontalws120b)<(timeup10+timehorizontalws10b) && (timeup120+timehorizontalws120b)<(timeup80+timehorizontalws80b))
-durationmin=durationmin+timeup120+timehorizontalws120b
-
-    stat = dist.toFixed(2)+','+speedhorizontal.toFixed(2)+','+ws120.toFixed(2)+','+diffangle.toFixed(2)+':'+
-                 'wind speed (@80m) '+ ws80.toFixed(2) +' m/s ' + wd80 + ' degrees\n' +
-                 'distance: '+dist.toFixed(2)+ ' meters\n'+
-                 'no wind:'+(timeup80+timehorizontal).toFixed(2)+ ' sec\n'+
-                 '(@10m): '+(timeup10+timehorizontalws10).toFixed(2)+ ' sec\n\r'+
-                 '(80m): '+(timeup80+timehorizontalws80).toFixed(2)+ ' sec\n\r'+
-                 '(@120m): '+(timeup120+timehorizontalws120).toFixed(2)+ ' sec\n'+
-                 'and back\n'+
-                 '(@10m): '+(timeup10+timehorizontalws10b).toFixed(2)+ ' sec\n'+
-                 '(@80m): '+(timeup80+timehorizontalws80b).toFixed(2)+ ' sec\n'+
-                 '(@120m): '+(timeup120+timehorizontalws120b).toFixed(2)+ ' sec \n'+' duration '+durationmin.toFixed(2)+' instead of'+duration80.toFixed(2)+'>'+(1-(duration80-durationmin)/duration80).toFixed(2)
-xa.innerHTML = stat;
-    return stat;
+    document.getElementById('savesec').innerHTML = (travel20-travelopt).toFixed(2)
+    document.getElementById('savemeters').innerHTML = ((travel20-travelopt)*speedhorizontal).toFixed(2)
+    return;
 }
 
 
